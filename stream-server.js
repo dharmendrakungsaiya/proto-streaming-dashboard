2a86f392ff7d92154208b3e0944db3d12a06f596
Dharmendra Kungsaiya <dharmendra.kungsaiya20@gmail.com>
2025-11-22 16:43:40 +0530

    RTSP stream link updated

--- stream-server.js
+++ stream-server.js
@@ -7,13 +7,11 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
-// 1. Setup Output Directory (Outside 'public' to stop reloads)
 const hlsDir = path.join(__dirname, 'hls_output');
 if (!fs.existsSync(hlsDir)){
     fs.mkdirSync(hlsDir, { recursive: true });
 }
 
-// Serve files on Port 4000
 app.use('/hls', express.static(hlsDir));
 
 const PORT = 4000;
@@ -21,18 +19,16 @@ app.listen(PORT, () => {
     console.log(`Media Server running on http://localhost:${PORT}`);
 });
 
-// 2. FFmpeg Configuration
-// The RTSP URL from the assignment
-const RTSP_URL = 'rtsp://13.60.76.79:8554/live'; 
+
+const RTSP_URL = 'rtsp://13.60.76.79:8554/live2'; 
 const ffmpegPath = path.join(__dirname, 'ffmpeg.exe');
 
 console.log(`Attempting to connect to RTSP Source: ${RTSP_URL}`);
 
 const ffmpeg = spawn(ffmpegPath, [
-    // INPUT: The RTSP Stream
-    '-i', RTSP_URL,  // <--- Make sure this comma exists!
+   
+    '-i', RTSP_URL,  
     
-    // Split into 5 streams
     '-filter_complex', '[0:v]split=5[v1][v2][v3][v4][v5]',
     
     // Stream 1
@@ -62,8 +58,7 @@ const ffmpeg = spawn(ffmpegPath, [
 ]);
 
 ffmpeg.stderr.on('data', (data) => {
-    // Uncomment this line to see error details if the RTSP link fails
-    // console.log(`FFmpeg: ${data}`);
+    console.log(`FFmpeg: ${data}`);
 });
 
 ffmpeg.on('close', (code) => {
